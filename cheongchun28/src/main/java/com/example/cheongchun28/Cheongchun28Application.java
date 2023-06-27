6f88ad04f53e3ac89660b29bc89a10e8eb3083ac
starMinK <rbals040329@gmail.com>
2023-06-27 20:41:13 +0900

    build: build init
    Building the initial project directory structure

---
+++ cheongchun28/src/main/java/com/example/cheongchun28/Cheongchun28Application.java
@@ -0,0 +1,13 @@
+package com.example.cheongchun28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cheongchun28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cheongchun28Application.class, args);
+    }
+
+}
---
+++ cheongchun28/src/test/java/com/example/cheongchun28/Cheongchun28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheongchun28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cheongchun28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
