2a627eeda522c47ec8b4341b46c03d6e35ec8ff7
jangheeda <woojang95@gmail.com>
2023-07-07 13:57:29 +0900

    feat: 로그인, 회원가입 TEST API 작성 #4
    Co-authored-by: jhreposi <jhsygo@gmail.com>

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,30 +1,25 @@
-// //let userDB : any = [];
+import { userDB } from "../store";
 
-// export const POST = async (request: Request) => {
-//     try {
-//         const {email} = await request.json();
-//         // EmailService.sendEmail(email, randomNumber);
-//         // email인증센터 = [...,{email, rand}];
-//         for (const user of userDB) {
-//             if (user.email == email) {
-//                 return new Response("이미 가입한 회원입니다..", {status:401});
-//             }
-//         }
-//         const randomNumber = Math.floor(Math.random() * 10000 % 1000);
-//         emailVerification = [...emailVerification, {email, code:randomNumber}];
-//         console.log(email, randomNumber);
-
-//         return new Response("전송 완료.", {status:201});
-//     } catch (error) {
-//        // console.log(error);
-//         return new Response("Failed to create a new prompt", { status: 500 });
-//     }
-// }
-
-// export const GET = async () => {
-//     try {
-//             return new Response(JSON.stringify({a:"get요청을하셨네요", b:"비입니다"}),{ status: 201 })
-//     }  catch (error) {
-//         return new Response("Failed to create a new prompt", { status: 500 });
-//     }
-// }
+export const POST = async (request: Request) => {
+  try {
+    const { email } = await request.json();
+    const result = userDB.emailExist({ email });
+    if (result == true) {
+      return new Response(
+        JSON.stringify({ message: "이미 존재하는 이메일입니다. " }),
+        {
+          status: 401,
+        }
+      );
+    }
+    console.log(userDB);
+    return new Response(
+      JSON.stringify({ message: "가입 가능한 이메일입니다." }),
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 });
+  }
+};
