5b9bcd510283dc382e8a021962d0a6b6da44a49a
dlraud3 <132827203+dlraud3@users.noreply.github.com>
2023-08-10 07:40:28 +0900

    Feat:"전체 예약 내역 조회 추가"

--- cheongchun28/src/main/java/com/example/cheongchun28/domain/admin/controller/AdminController.java
+++ cheongchun28/src/main/java/com/example/cheongchun28/domain/admin/controller/AdminController.java
@@ -2,13 +2,17 @@ package com.example.cheongchun28.domain.admin.controller;
 
 import com.example.cheongchun28.domain.admin.dto.AdminDto;
 import com.example.cheongchun28.domain.admin.service.AdminService;
+import com.example.cheongchun28.domain.reservation.dto.ReservationResponseDto;
 import com.example.cheongchun28.global.common.dto.CustomResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -42,5 +46,10 @@ public class AdminController {
     @PutMapping("/user")
     public CustomResponseDto setUser(@RequestBody AdminDto.setUserRequestDto requestDto) throws SQLException {
         return adminService.setUser(requestDto);
+    }
+
+    @GetMapping("/reservation")
+    public List<ReservationResponseDto.ReservationAllResponseDto> getAllReservations() {
+        return adminService.getAllReservations();
     }
 }
--- cheongchun28/src/main/java/com/example/cheongchun28/domain/admin/service/AdminService.java
+++ cheongchun28/src/main/java/com/example/cheongchun28/domain/admin/service/AdminService.java
@@ -1,6 +1,9 @@
 package com.example.cheongchun28.domain.admin.service;
 
 import com.example.cheongchun28.domain.admin.dto.AdminDto;
+import com.example.cheongchun28.domain.reservation.dto.ReservationResponseDto;
+import com.example.cheongchun28.domain.reservation.entity.Reservation;
+import com.example.cheongchun28.domain.reservation.repository.ReservationRepository;
 import com.example.cheongchun28.domain.user.entity.User;
 import com.example.cheongchun28.domain.user.repository.UserRepository;
 import com.example.cheongchun28.global.common.dto.CustomResponseDto;
@@ -10,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +23,7 @@ import java.sql.SQLException;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final ReservationRepository reservationRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public CustomResponseDto setPermission(AdminDto.setPermissionRequestDto requestDto) throws SQLException {
@@ -50,4 +57,35 @@ public class AdminService {
         userRepository.save(user);
         return new CustomResponseDto(200);
     }
+
+    public List<ReservationResponseDto.ReservationAllResponseDto> getAllReservations() {
+        List<Reservation> reservations = reservationRepository.findAll();
+        List<ReservationResponseDto.ReservationAllResponseDto> reservationAllResponseDto = new ArrayList<>();
+        for (Reservation reservation : reservations) {
+            List<String> resUser = reservation.getRoom()
+                    .getReservations()
+                    .stream()
+                    .map(res -> res.getUser().getNickName())
+                    .collect(Collectors.toList());
+
+            ReservationResponseDto.ReservationAllResponseDto response = ReservationResponseDto.ReservationAllResponseDto
+                    .builder()
+                    .nickName(reservation.getUser().getNickName())
+                    .roomName(reservation.getRoom().getRoomName())
+                    .resUser(resUser)
+                    .startTime(reservation.getStartDate())
+                    .endTime(reservation.getEndDate())
+                    .build();
+//                    new ReservationResponseDto.ReservationAllResponseDto(
+//                    reservation.getUser().getNickName(),
+//                    reservation.getRoom().getRoomName(),
+//                    resUser,
+//                    reservation.getStartDate(),
+//                    reservation.getEndDate()
+//            );
+           reservationAllResponseDto.add(response);
+        }
+        return reservationAllResponseDto;
+    }
+
 }
