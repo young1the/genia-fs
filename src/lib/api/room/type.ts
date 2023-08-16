da8bac0d44ae851a4ea314dff583402ea7a2fadf
young1the <youngthedev@gmail.com>
2023-08-16 21:47:52 +0900

    fix: 강의실의 현재 예약 받아오기

--- src/lib/api/room/type.ts
+++ src/lib/api/room/type.ts
@@ -7,6 +7,9 @@ export type Room = {
   blackBoard: number;
   computer: number;
   reservationCode: ReservationCode;
+  topic: string;
+  startDate: string;
+  endDate: string;
 };
 
 export interface RoomId extends Pick<Room, "id"> {}
